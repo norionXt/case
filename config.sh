1284c7986e15e7fc109588653e8f11968a9a39b9
norionXt <93691381+norionXt@users.noreply.github.com>
2023-05-10 05:52:43 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ docker exec -i case_mysql mysql -uroot -p123 myapp < config/mysql/init.sql
 
 
 # Adiciona a atividade no cron
-echo "0 * * * * /src/System/Modules/Jobs/Jobs.php" >> /etc/crontab
+#echo "0 * * * * /src/System/Modules/Jobs/Jobs.php" >> /etc/crontab
 
 # Reinicia o serviço cron para aplicar as mudanças
-service cron restart
+#service cron restart
